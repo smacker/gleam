8eb8edb60f6871b645c3509125e606bbe28f335c
Maxim Sukharev <maxim@sourced.tech>
2018-01-28 23:54:04 +0100

    add head hash to repo and fix infinity loop

--- plugins/git/repositories/repositories_git_reader.go
+++ plugins/git/repositories/repositories_git_reader.go
@@ -1,6 +1,10 @@
 package repositories
 
 import (
+	"io"
+
+	"gopkg.in/src-d/go-git.v4/plumbing"
+
 	"github.com/chrislusf/gleam/util"
 	git "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/storer"
@@ -12,12 +16,15 @@ type RepositoriesGitReader struct {
 	repositoryID string
 	urls         []string
 	refs         storer.ReferenceIter
+	head         *plumbing.Reference
+	hasBeenRead  bool
 }
 
 func New(r *git.Repository, path string) *RepositoriesGitReader {
 
 	refs, _ := r.References()
 	remotes, _ := r.Remotes()
+	head, _ := r.Head()
 	urls := remotes[0].Config().URLs
 
 	return &RepositoriesGitReader{
@@ -25,6 +32,7 @@ func New(r *git.Repository, path string) *RepositoriesGitReader {
 		repositoryID: path,
 		urls:         urls,
 		refs:         refs,
+		head:         head,
 	}
 }
 
@@ -33,12 +41,16 @@ func (r *RepositoriesGitReader) ReadHeader() (fieldNames []string, err error) {
 		"repositoryID",
 		"repositoryPath",
 		"repositoryURLs",
+		"headHash",
 	}
 	return fieldNames, nil
 }
 
 //TODO: add is_fork
 func (r *RepositoriesGitReader) Read() (row *util.Row, err error) {
-
-	return util.NewRow(util.Now(), r.repositoryID, r.urls), nil
+	if r.hasBeenRead {
+		return nil, io.EOF
+	}
+	r.hasBeenRead = true
+	return util.NewRow(util.Now(), r.repositoryID, r.urls, r.head.Hash().String()), nil
 }

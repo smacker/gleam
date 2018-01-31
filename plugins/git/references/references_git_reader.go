e5ca13c3de2596a2ec5ba14d06bce7a23f13330d
Maxim Sukharev <maxim@sourced.tech>
2018-01-31 21:10:58 +0100

    Implement correct reference reader
    * Return commit hash
    
    Engine hides real reference hash and always returns commit hash, but
    here I return both of them.
    
    Resolving correct commit hash is a bit tricky with go git.
    Reference can by symbolic and it can be a tag.
    I handle both cases and validated results using engine.
    
    * Add isRemote flag

--- plugins/git/references/references_git_reader.go
+++ plugins/git/references/references_git_reader.go
@@ -1,14 +1,18 @@
 package references
 
 import (
+	"strconv"
+
 	"github.com/chrislusf/gleam/util"
 	"github.com/pkg/errors"
 	git "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 	storer "gopkg.in/src-d/go-git.v4/plumbing/storer"
 )
 
 type Reader struct {
 	repositoryID string
+	repo         *git.Repository
 	refs         storer.ReferenceIter
 }
 
@@ -19,6 +23,7 @@ func NewReader(repo *git.Repository, path string) (*Reader, error) {
 	}
 	return &Reader{
 		repositoryID: path,
+		repo:         repo,
 		refs:         refs,
 	}, nil
 }
@@ -29,15 +34,38 @@ func (r *Reader) ReadHeader() ([]string, error) {
 		"refHash",
 		"refName",
 		"commitHash",
+		"isRemote",
 	}, nil
 }
 
-//TODO: add is_remote
 func (r *Reader) Read() (*util.Row, error) {
 	ref, err := r.refs.Next()
 	if err != nil {
 		return nil, err
 	}
 
-	return util.NewRow(util.Now(), r.repositoryID, ref.Hash().String(), ref.Name().String()), nil
+	// Get correct commit hash
+	// there is Repository.ResolveRevision but it fails on some tags and performance is worst
+
+	commitHash := ref.Hash()
+	// handle symbolic references like HEAD
+	if ref.Type() == plumbing.SymbolicReference {
+		targetRef, _ := r.repo.Reference(ref.Target(), true)
+		commitHash = targetRef.Hash()
+	}
+
+	// handle tag references
+	tag, err := r.repo.TagObject(commitHash)
+	if err == nil {
+		commit, _ := tag.Commit()
+		commitHash = commit.Hash
+	}
+
+	return util.NewRow(util.Now(),
+		r.repositoryID,
+		ref.Hash().String(),
+		ref.Name().String(),
+		commitHash.String(),
+		strconv.FormatBool(ref.Name().IsRemote()),
+	), nil
 }

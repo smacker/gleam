24f17f20b3bbcae3d3e6426c22c8a7a0d0d202a7
Maxim Sukharev <maxim@sourced.tech>
2018-01-31 23:46:48 +0100

    Implement children in references reader

--- plugins/git/references/references_git_reader.go
+++ plugins/git/references/references_git_reader.go
@@ -1,12 +1,14 @@
 package references
 
 import (
+	"io"
 	"strconv"
 
 	"github.com/chrislusf/gleam/util"
 	"github.com/pkg/errors"
 	git "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 	storer "gopkg.in/src-d/go-git.v4/plumbing/storer"
 )
 
@@ -14,6 +16,11 @@ type Reader struct {
 	repositoryID string
 	repo         *git.Repository
 	refs         storer.ReferenceIter
+	commitsIter  object.CommitIter
+
+	refCommitHash string
+	refName       string
+	refIsRemote   string
 }
 
 func NewReader(repo *git.Repository, path string) (*Reader, error) {
@@ -39,33 +46,52 @@ func (r *Reader) ReadHeader() ([]string, error) {
 }
 
 func (r *Reader) Read() (*util.Row, error) {
-	ref, err := r.refs.Next()
+	if r.commitsIter == nil {
+		ref, err := r.refs.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		// Get correct commit hash
+		// there is Repository.ResolveRevision but it fails on some tags and performance is worst
+		refCommitHash := ref.Hash()
+		// handle symbolic references like HEAD
+		if ref.Type() == plumbing.SymbolicReference {
+			targetRef, _ := r.repo.Reference(ref.Target(), true)
+			refCommitHash = targetRef.Hash()
+		}
+
+		// handle tag references
+		tag, err := r.repo.TagObject(refCommitHash)
+		if err == nil {
+			commit, _ := tag.Commit()
+			refCommitHash = commit.Hash
+		}
+
+		r.commitsIter, err = r.repo.Log(&git.LogOptions{From: refCommitHash})
+		if err != nil {
+			return nil, err
+		}
+
+		r.refCommitHash = refCommitHash.String()
+		r.refName = ref.Name().String()
+		r.refIsRemote = strconv.FormatBool(ref.Name().IsRemote())
+	}
+
+	commit, err := r.commitsIter.Next()
+	if err == io.EOF {
+		r.commitsIter = nil
+		return nil, nil
+	}
 	if err != nil {
 		return nil, err
 	}
 
-	// Get correct commit hash
-	// there is Repository.ResolveRevision but it fails on some tags and performance is worst
-
-	commitHash := ref.Hash()
-	// handle symbolic references like HEAD
-	if ref.Type() == plumbing.SymbolicReference {
-		targetRef, _ := r.repo.Reference(ref.Target(), true)
-		commitHash = targetRef.Hash()
-	}
-
-	// handle tag references
-	tag, err := r.repo.TagObject(commitHash)
-	if err == nil {
-		commit, _ := tag.Commit()
-		commitHash = commit.Hash
-	}
-
 	return util.NewRow(util.Now(),
 		r.repositoryID,
-		ref.Hash().String(),
-		ref.Name().String(),
-		commitHash.String(),
-		strconv.FormatBool(ref.Name().IsRemote()),
+		r.refCommitHash,
+		r.refName,
+		commit.Hash.String(),
+		r.refIsRemote,
 	), nil
 }
